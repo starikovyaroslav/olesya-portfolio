4305dd4e0582f199dff73f1030ab37732f7f230f
starikovyaroslav <slava.com.com90@gmail.com>
2022-04-24 22:21:39 +0700

    feat: сделан NavTav

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -5,11 +5,11 @@ export default function NavTab({onStatus}) {
 
   return (
     <nav className="nav">
-      <ul>
-        <li className="nav__link" onClick={() => onStatus("all")}>Все</li>
-        <li className="nav__link" onClick={() => onStatus("portraits")}>Портреты</li>
-        <li className="nav__link" onClick={() => onStatus("studio")}>Студийные</li>
-        <li className="nav__link" onClick={() => onStatus("love")}>Love story</li>
+      <ul className='nav__items'>
+        <li className="nav__item" onClick={() => onStatus("all")}>Все</li>
+        <li className="nav__item" onClick={() => onStatus("portraits")}>Портреты</li>
+        <li className="nav__item" onClick={() => onStatus("studio")}>Студийные</li>
+        <li className="nav__item" onClick={() => onStatus("love")}>Love story</li>
       </ul>
     </nav>
   )
