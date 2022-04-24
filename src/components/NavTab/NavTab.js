51c6e04fe422ebdadb2173ee86a869f5af24560a
starikovyaroslav <slava.com.com90@gmail.com>
2022-04-24 22:54:40 +0700

    feat: NavTab адаптив

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -4,13 +4,21 @@ import './NavTab.css';
 export default function NavTab({onStatus}) {
 
   return (
-    <nav className="nav">
-      <ul className='nav__items'>
-        <li className="nav__item" onClick={() => onStatus("all")}>Все</li>
-        <li className="nav__item" onClick={() => onStatus("portraits")}>Портреты</li>
-        <li className="nav__item" onClick={() => onStatus("studio")}>Студийные</li>
-        <li className="nav__item" onClick={() => onStatus("love")}>Love story</li>
-      </ul>
-    </nav>
+    <>
+      <nav className="nav">
+        <ul className='nav__items'>
+          <li className="nav__item" onClick={() => onStatus("all")}>Все</li>
+          <li className="nav__item" onClick={() => onStatus("portraits")}>Портреты</li>
+          <li className="nav__item" onClick={() => onStatus("studio")}>Студийные</li>
+          <li className="nav__item" onClick={() => onStatus("love")}>Love story</li>
+        </ul>
+      </nav>
+      <select className='select'>
+          <option className="select__item" onClick={() => onStatus("all")}>Все</option>
+          <option className="select__item" onClick={() => onStatus("portraits")}>Портреты</option>
+          <option className="select__item" onClick={() => onStatus("studio")}>Студийные</option>
+          <option className="select__item" onClick={() => onStatus("love")}>Love story</option>
+      </select>
+    </>
   )
 }
