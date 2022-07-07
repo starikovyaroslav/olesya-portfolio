b2923eb441b02753f03e2457c2d3bf5f4b598911
starikovyaroslav <slava.com.com90@gmail.com>
2022-07-07 23:09:14 +0700

    feat: add prices

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { Route, Routes, useNavigate, useLocation, Navigate} from "react-router-dom";
 import { Main } from "../Main/Main";
 import { Portfolio } from "../Portfolio/Portfolio";
+import { Prices } from "../Prices/Prices";
 
 function App() {
 
@@ -12,6 +13,7 @@ return (
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route exact path="/portfolio" element={<Portfolio />} />
+        <Route exact path="/price" element={<Prices />} />
       </Routes>
     </div>
 );
